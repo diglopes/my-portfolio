548d42f250d2306d81ffa9bded919686df4938f2
Diego Lopes <diego-chez@hotmail.com>
2020-05-12 17:47:06 -0300

    Change article when click on navigation item

---
+++ script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll(".nav-item");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", ({ currentTarget }) => {
+    changeActiveMenuItem(currentTarget);
+    const { article } = currentTarget.dataset;
+    removeCurrentArticle();
+    showRelatedArticle(article);
+  });
+});
+
+function removeCurrentArticle() {
+  const article = document.querySelector(".show");
+  if (article) {
+    article.classList.remove("show");
+    article.classList.add("hide");
+  }
+}
+
+function showRelatedArticle(selector) {
+  const article = document.getElementById(selector);
+  article.classList.remove("hide");
+  article.classList.add("show");
+}
+
+function changeActiveMenuItem(currentElement) {
+  const oldActive = document.querySelector(".active");
+  oldActive.classList.remove("active");
+  currentElement.classList.add("active");
+}
